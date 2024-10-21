5ca03e56814e0b57501676fe667b6092179b2fde
Kiratipat sawangsisombat <capacity1412@gmail.com>
2024-10-21 14:19:55 +0700

    ng g class shared/validators/th-mobile.validator

---
+++ src/app/shared/validators/th-mobile.validator.spec.ts
@@ -0,0 +1,7 @@
+import { ThMobileValidator } from './th-mobile.validator';
+
+describe('ThMobileValidator', () => {
+  it('should create an instance', () => {
+    expect(new ThMobileValidator()).toBeTruthy();
+  });
+});

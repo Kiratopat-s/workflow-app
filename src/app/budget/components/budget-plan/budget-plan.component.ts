531f653a377a3339fe63bf60ac4f1b7d59f0388b
Kiratipat sawangsisombat <capacity1412@gmail.com>
2024-10-21 14:49:09 +0700

    Refactor BudgetPlanComponent to include editable property and update available budget based on a percentage

--- src/app/budget/components/budget-plan/budget-plan.component.ts
+++ src/app/budget/components/budget-plan/budget-plan.component.ts
@@ -1,12 +1,33 @@
-import { Component } from '@angular/core';
+import { DecimalPipe } from '@angular/common';
+import { booleanAttribute, Component, inject, Input } from '@angular/core';
+import { FormControl, ReactiveFormsModule } from '@angular/forms';
+import { startWith } from 'rxjs';
+import { BudgetPlanService } from '../../budget-plan.service';
 
 @Component({
   selector: 'app-budget-plan',
   standalone: true,
-  imports: [],
+  imports: [DecimalPipe, ReactiveFormsModule],
   templateUrl: './budget-plan.component.html',
   styleUrl: './budget-plan.component.scss'
 })
 export class BudgetPlanComponent {
+  @Input({ transform: booleanAttribute })
+  editable: boolean = false;
 
+  budgetPlanService = inject(BudgetPlanService);
+  budgetPlan = this.budgetPlanService.budgetPlan;
+  balance = this.budgetPlanService.balance;
+
+  availablePercent = new FormControl<number>(this.budgetPlanService.DEFAULT_AVAILABLE_PERCENT, {
+    nonNullable: true
+  });
+
+  constructor() {
+    this.availablePercent.valueChanges
+      .pipe(startWith(this.availablePercent.value))
+      .subscribe((percent) => {
+        this.budgetPlanService.updateAvailable(percent);
+      });
+  }
 }

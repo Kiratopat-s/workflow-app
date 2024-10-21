ef45add936dd3223b4daefae5bd1d80f2df7e0b0
Kiratipat sawangsisombat <capacity1412@gmail.com>
2024-10-21 14:03:33 +0700

    Refactor item-entry component to display item details in a table and add a filter input

--- src/app/budget/pages/item-entry/item-entry.component.ts
+++ src/app/budget/pages/item-entry/item-entry.component.ts
@@ -1,41 +1,54 @@
-import { FormsModule } from '@angular/forms';
 import { Component } from '@angular/core';
+import { FormControl, FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { map } from 'rxjs';
 import { Item, ItemStatus } from '../../models/item';
 
 @Component({
   selector: 'app-item-entry',
   standalone: true,
-  imports: [FormsModule],
+  imports: [FormsModule, ReactiveFormsModule],
   templateUrl: './item-entry.component.html',
   styleUrl: './item-entry.component.scss'
 })
 export class ItemEntryComponent {
   items: Item[] = [
     {
-      title: "Gaming Laptop",
+      title: 'Gaming Laptop',
       amount: 3,
       price: 3200,
-      contactMobileNo: "0812345678",
+      contactMobileNo: '0812345678',
       status: ItemStatus.APPROVED,
       id: 1
     },
     {
-      title: "Desktop Tower",
+      title: 'Desktop Tower',
       amount: 7,
       price: 2500,
-      contactMobileNo: "0823456789",
+      contactMobileNo: '0823456789',
       status: ItemStatus.PENDING,
       id: 2
     },
     {
-      title: "Mechanical Keyboard",
+      title: 'Mechanical Keyboard',
       amount: 6,
       price: 750,
-      contactMobileNo: "0834567890",
+      contactMobileNo: '0834567890',
       status: ItemStatus.REJECTED,
       id: 3
     }
   ];
 
   isSmallTable = false;
+  filterItems = this.items;
+
+  filterInput = new FormControl<string>('', { nonNullable: true });
+
+  constructor() {
+    this.filterInput.valueChanges // ดักเหตุการณ์ที่ value เปลี่ยนได้
+      .pipe(map((keyword) => keyword.toLocaleLowerCase())) // convert value ได้
+      .subscribe((keyword) => {
+
+        this.filterItems = this.items.filter((item) => item.title.toLocaleLowerCase().includes(keyword)); // เขียน logic จากการเปลี่ยน value ได้
+      });
+  }
 }

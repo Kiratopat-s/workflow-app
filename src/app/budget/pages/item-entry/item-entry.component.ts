b8b94ebedf775569948a15d9e19b8c0718939fc7
Kiratipat sawangsisombat <capacity1412@gmail.com>
2024-10-21 14:41:29 +0700

    Refactor item-entry.component.html and item-entry.component.ts

--- src/app/budget/pages/item-entry/item-entry.component.ts
+++ src/app/budget/pages/item-entry/item-entry.component.ts
@@ -1,51 +1,64 @@
-import { Component, inject } from '@angular/core';
+import { Component, inject, TemplateRef } from '@angular/core';
 import { FormControl, FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { map } from 'rxjs';
-import { Item, ItemStatus } from '../../models/item';
-import { HttpClient } from '@angular/common/http';
 import { ItemService } from '../../item.service';
+import { Item } from '../../models/item';
 import { MobileFormatPipe } from '../../../shared/pipes/mobile-format.pipe';
 import { DecimalPipe } from '@angular/common';
 import { RouterLink } from '@angular/router';
+import { BsModalRef, BsModalService, ModalOptions } from 'ngx-bootstrap/modal';
+import { ConfirmModalComponent } from '../../../shared/components/confirm-modal/confirm-modal.component';
 
 @Component({
   selector: 'app-item-entry',
   standalone: true,
-  imports: [FormsModule, ReactiveFormsModule, MobileFormatPipe, DecimalPipe, RouterLink],
+  imports: [FormsModule, ReactiveFormsModule, MobileFormatPipe, DecimalPipe, RouterLink], // add
   templateUrl: './item-entry.component.html',
   styleUrl: './item-entry.component.scss'
 })
 export class ItemEntryComponent {
+  isSmallTable = false;
 
-
-  itemService = inject(ItemService)
+  itemService = inject(ItemService);
 
   items: Item[] = [];
-
-  isSmallTable = false;
   filterItems = this.items;
-
   filterInput = new FormControl<string>('', { nonNullable: true });
 
-  constructor() {
+  modalService = inject(BsModalService)
+  bsModalRef?: BsModalRef;
 
-    this.itemService.list().subscribe(vs => {
+  constructor() {
+    this.itemService.list().subscribe((vs) => {
       this.items = vs;
       this.filterItems = vs;
-    })
+    });
 
-    this.filterInput.valueChanges // ดักเหตุการณ์ที่ value เปลี่ยนได้
-      .pipe(map((keyword) => keyword.toLocaleLowerCase())) // convert value ได้
+    this.filterInput.valueChanges
+      .pipe(map((keyword) => keyword.toLocaleLowerCase()))
       .subscribe((keyword) => {
-        console.log('keyword', keyword)
-        this.filterItems = this.items.filter((item) => item.title.toLocaleLowerCase().includes(keyword)); // เขียน logic จากการเปลี่ยน value ได้
+        this.filterItems = this.items.filter((item) =>
+          item.title.toLocaleLowerCase().includes(keyword)
+        );
       });
   }
 
+  onConfirm(item: Item) {
+    const initialState: ModalOptions = {
+      initialState: {
+        title: `Confirm to delete "${item.title}" ?`
+      }
+    };
+    this.bsModalRef = this.modalService.show(ConfirmModalComponent, initialState);
+    this.bsModalRef?.onHidden?.subscribe(() => {
+      if (this.bsModalRef?.content?.confirmed) {
+        this.onDelete(item.id)
+      }
+    })
+
+  }
+
   onDelete(id: number) {
-    return this.itemService.delete(id).subscribe(v => {
-      this.items = this.items.filter(item => item.id != id)
-      this.filterItems = this.items
-    });
+    this.itemService.delete(id).subscribe(() => this.filterItems = this.filterItems.filter(v => v.id != id));
   }
 }

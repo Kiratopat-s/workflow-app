095da6b714e83543c5242344b58271ab61048013
Kiratipat sawangsisombat <capacity1412@gmail.com>
2024-10-21 14:12:31 +0700

    Refactor mobile-format.pipe.ts to format mobile numbers with dashes

--- src/app/shared/pipes/mobile-format.pipe.ts
+++ src/app/shared/pipes/mobile-format.pipe.ts
@@ -6,8 +6,11 @@ import { Pipe, PipeTransform } from '@angular/core';
 })
 export class MobileFormatPipe implements PipeTransform {
 
-  transform(value: unknown, ...args: unknown[]): unknown {
-    return null;
+  transform(value: string): string {
+    const m1 = value.slice(0, 3)
+    const m2 = value.slice(3, 6)
+    const m3 = value.slice(6)
+    return `${m1}-${m2}-${m3}`
   }
 
 }

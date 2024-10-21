29c2a5d8c6744a015b1426a3b5b818c0fdf4b33c
Kiratipat sawangsisombat <capacity1412@gmail.com>
2024-10-21 14:48:38 +0700

    ng g cbudget/components/budget-plan --skip-tests

---
+++ src/app/budget/components/budget-plan/budget-plan.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-budget-plan',
+  standalone: true,
+  imports: [],
+  templateUrl: './budget-plan.component.html',
+  styleUrl: './budget-plan.component.scss'
+})
+export class BudgetPlanComponent {
+
+}

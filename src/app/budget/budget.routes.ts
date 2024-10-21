a07fcb5e682d704ce1897299d4b7af61c5e99e54
Kiratipat sawangsisombat <capacity1412@gmail.com>
2024-10-21 14:14:11 +0700

    Refactor budget.routes.ts to add routes for item entry and item form components

--- src/app/budget/budget.routes.ts
+++ src/app/budget/budget.routes.ts
@@ -1,2 +1,10 @@
-export class BudgetRoutes {
-}
+import { Routes } from '@angular/router';
+import { ItemEntryComponent } from './pages/item-entry/item-entry.component';
+import { ItemFormComponent } from './pages/item-form/item-form.component';
+
+export const routes: Routes = [
+  { path: 'item-entry', component: ItemEntryComponent, title: 'Entry' },
+  { path: 'item-add', component: ItemFormComponent, title: 'Add' }
+];
+
+export default routes;

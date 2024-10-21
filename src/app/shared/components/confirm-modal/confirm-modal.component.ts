fb0f9d3eae59fb2a380a3f8978488ba2526c8712
Kiratipat sawangsisombat <capacity1412@gmail.com>
2024-10-21 14:41:34 +0700

    Refactor confirm-modal component to update the HTML template and add confirmation functionality

--- src/app/shared/components/confirm-modal/confirm-modal.component.ts
+++ src/app/shared/components/confirm-modal/confirm-modal.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { BsModalRef } from 'ngx-bootstrap/modal';
 
 @Component({
   selector: 'app-confirm-modal',
@@ -8,5 +9,18 @@ import { Component } from '@angular/core';
   styleUrl: './confirm-modal.component.scss'
 })
 export class ConfirmModalComponent {
+  title = 'Do you want to confirm?';
+  confirmed = false;
 
+  constructor(public bsModalRef: BsModalRef) { }
+
+  confirm(): void {
+    this.confirmed = true;
+    this.bsModalRef?.hide();
+  }
+
+  decline(): void {
+    this.confirmed = false;
+    this.bsModalRef?.hide();
+  }
 }

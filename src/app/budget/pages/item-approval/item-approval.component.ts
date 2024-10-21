2605a43b6b3268865010ccec3bec56eee439d1fe
Kiratipat sawangsisombat <capacity1412@gmail.com>
2024-10-21 14:57:52 +0700

    Refactor BudgetPlanService to update used state

--- src/app/budget/pages/item-approval/item-approval.component.ts
+++ src/app/budget/pages/item-approval/item-approval.component.ts
@@ -7,18 +7,20 @@ import { MobileFormatPipe } from '../../../shared/pipes/mobile-format.pipe';
 import { ItemService } from '../../item.service';
 import { Item, ItemStatus } from '../../models/item';
 import { BudgetPlanComponent } from '../../components/budget-plan/budget-plan.component';
+import { BudgetPlanService } from '../../budget-plan.service';
 
 type ItemAction = 'Approve' | 'Reject';
 
 @Component({
   selector: 'app-item-approval',
   standalone: true,
-  imports: [ReactiveFormsModule, DecimalPipe, MobileFormatPipe, BudgetPlanComponent], // add
+  imports: [ReactiveFormsModule, DecimalPipe, MobileFormatPipe, BudgetPlanComponent],
   templateUrl: './item-approval.component.html',
   styleUrl: './item-approval.component.scss'
 })
 export class ItemApprovalComponent {
   itemService = inject(ItemService);
+  budgetPlanService = inject(BudgetPlanService)
 
   items: Item[] = [];
 
@@ -30,6 +32,7 @@ export class ItemApprovalComponent {
   constructor() {
     this.itemService.list().subscribe((vs) => {
       this.items = vs;
+      this.updateUsed();
     });
   }
 
@@ -54,16 +57,32 @@ export class ItemApprovalComponent {
   }
 
   onApprove(id: number) {
-    // in api
     this.itemService.approve(id).subscribe(() => {
-      // in front-end
       this.items = this.items.map((v) => (v.id === id ? { ...v, status: ItemStatus.APPROVED } : v));
+      this.updateUsed();
     });
   }
 
   onReject(id: number) {
     this.itemService.reject(id).subscribe(() => {
       this.items = this.items.map((v) => (v.id === id ? { ...v, status: ItemStatus.REJECTED } : v));
+      this.updateUsed();
     });
   }
+
+  private updateUsed() {
+    const used = this.items
+      .filter((v) => v.status === ItemStatus.APPROVED) // [{ id: 5, price: 600, ... }, { id: 8, price: 1200, ... }]
+      .map((v) => v.price) // [600, 1200]
+      .reduce((previos, current) => (previos += current), 0);
+
+    this.budgetPlanService.updateUsed(used);
+
+    // reduce (sumFn, initValue)
+    // sumFn = (previos, current) => (previos += current)
+    // 1. (0, 600) => (0 += 600) then return to previos
+    // 2. (600, 1200) => (600 + 1200) return to previos
+    // 3. return 1800 as previos
+  }
+
 }

f53a4f658834e2162926495fec99cd9eb814854e
Kiratipat sawangsisombat <capacity1412@gmail.com>
2024-10-21 14:26:05 +0700

    Refactor item.ts to update CreateItem interface and add CreateItem type

--- src/app/budget/models/item.ts
+++ src/app/budget/models/item.ts
@@ -2,13 +2,15 @@ export enum ItemStatus {
   PENDING = 'PENDING', APPROVED = 'APPROVED', REJECTED = 'REJECTED'
 }
 
-export interface CreateItem {
-  title: string;
-  amount: number;
-  price: number;
-  contactMobileNo: string;
-  status: ItemStatus;
-}
+// export interface CreateItem {
+//   title: string;
+//   amount: number;
+//   price: number;
+//   contactMobileNo: string;
+//   status: ItemStatus;
+// }
+
+export type CreateItem = Omit<Item, "id">
 
 export interface Item {
   id: number;

1238b076460e6e95c4da9f3df0c2769ca2bf8500
Kiratipat sawangsisombat <capacity1412@gmail.com>
2024-10-22 12:35:32 +0700

    Refactor EnvConfig to use InjectionToken for environment configuration

--- src/app/env.config.ts
+++ src/app/env.config.ts
@@ -1,2 +1,11 @@
-export class EnvConfig {
-}
+import { InjectionToken } from "@angular/core";
+
+export type EnvConfig = {
+  apiUrl: string;
+};
+
+export const ENV_CONFIG = new InjectionToken<EnvConfig>('ENV_CONFIG', {
+  factory: () => {
+    return { apiUrl: 'http://localhost:3000' }
+  }
+})

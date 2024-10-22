fb7157f31225b9e672996673a6efe78a91958cc9
Kiratipat sawangsisombat <capacity1412@gmail.com>
2024-10-22 12:22:31 +0700

    Refactor app.routes.ts to add demo component route

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,5 +1,7 @@
 import { Routes } from '@angular/router';
+import { DemoComponent } from './demo/demo.component';
 
 export const routes: Routes = [
+  { path: 'demo', component: DemoComponent },
   { path: 'budget', loadChildren: () => import('./budget/budget.routes') }
 ];

ad16820eb9726468a8dd51794bab7604d704debc
Kiratipat sawangsisombat <capacity1412@gmail.com>
2024-10-22 12:43:01 +0700

    Refactor roles.guard.ts to include role-based authorization logic

--- src/app/auth/guards/roles.guard.ts
+++ src/app/auth/guards/roles.guard.ts
@@ -1,5 +1,18 @@
-import { CanActivateFn } from '@angular/router';
+import { CanActivateFn, Router } from '@angular/router';
+import { Role } from '../models/logged-in-user';
+import { AuthService } from '../auth.service';
+import { inject } from '@angular/core';
 
-export const rolesGuard: CanActivateFn = (route, state) => {
-  return true;
+export function rolesGuard(roles: Role[]): CanActivateFn {
+  return (route, state) => {
+
+    const loggedInUser = inject(AuthService).loggedInUser
+
+    if (loggedInUser && roles.includes(loggedInUser?.userProfile.role)) {
+      return true;
+    }
+
+    inject(Router).navigate(['/']);
+    return false;
+  }
 };

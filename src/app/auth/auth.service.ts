8b53d63ea7b292afac2da2b1c0ddb8979c2c3504
Kiratipat sawangsisombat <capacity1412@gmail.com>
2024-10-22 12:37:45 +0700

    Refactor AuthService to use environment configuration for API URL

--- src/app/auth/auth.service.ts
+++ src/app/auth/auth.service.ts
@@ -2,34 +2,37 @@ import { HttpClient } from '@angular/common/http';
 import { computed, inject, Injectable, signal } from '@angular/core';
 import { jwtDecode } from 'jwt-decode';
 import { Observable, tap } from 'rxjs';
+import { LoggedInUser, Tokens, UserProfile } from './models/logged-in-user';
 import { ENV_CONFIG } from '../env.config';
-import { LoggedInUser, Tokens } from './models/logged-in-user';
+import { Router } from '@angular/router';
 
 @Injectable({
   providedIn: 'root'
 })
 export class AuthService {
 
-  // change to use envConfig
   private envConfig = inject(ENV_CONFIG);
   readonly URL = `${this.envConfig.apiUrl}/auth/login`;
 
   httpClient = inject(HttpClient);
+  router = inject(Router) // add
 
-  tokens = signal<Tokens | null>(null);
-  loggedInUser = computed(() => {
-    const tokens = this.tokens();
-    if (tokens) {
-      return jwtDecode<LoggedInUser>(tokens.access_token);
-    }
-    return null;
-  });
+  loggedInUser: LoggedInUser | null = null;
 
   login(credential: { username: string; password: string }): Observable<Tokens> {
-    return this.httpClient.post<Tokens>(this.URL, credential).pipe(
-      tap((v) => {
-        this.tokens.set(v);
-      })
-    );
+    return this.httpClient
+      .post<Tokens>(this.URL, credential)
+      .pipe(tap((newToken) => this.setTokens(newToken)));
+  }
+
+  setTokens(newToken: Tokens) {
+    const userProfile = jwtDecode<UserProfile>(newToken.access_token);
+    this.loggedInUser = { tokens: newToken, userProfile };
+  }
+
+  // add
+  logout(): void {
+    this.loggedInUser = null;
+    this.router.navigate(['/auth/login']);
   }
 }

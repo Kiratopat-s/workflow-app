79fe35c21478f674978fb906cd8603036f0c6afd
Kiratipat sawangsisombat <capacity1412@gmail.com>
2024-10-22 12:44:50 +0700

    Refactor can-deactivate.guard.ts to implement CanComponentDeactivate interface

--- src/app/auth/guards/can-deactivate.guard.ts
+++ src/app/auth/guards/can-deactivate.guard.ts
@@ -1,5 +1,11 @@
+// can-deactivate.guard.ts
 import { CanDeactivateFn } from '@angular/router';
+import { Observable } from 'rxjs';
 
-export const canDeactivateGuard: CanDeactivateFn<unknown> = (component, currentRoute, currentState, nextState) => {
-  return true;
+export interface CanComponentDeactivate {
+  canDeactivate: () => boolean | Observable<boolean>;
+}
+
+export const canDeactivateGuard: CanDeactivateFn<CanComponentDeactivate> = (component: CanComponentDeactivate) => {
+  return component.canDeactivate ? component.canDeactivate() : true;
 };

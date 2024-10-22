dff9e8628e8e29233819be4301d81ef7e7a45899
Kiratipat sawangsisombat <capacity1412@gmail.com>
2024-10-22 12:43:56 +0700

    Refactor budget.routes.ts to add role-based authorization for the 'item-approval' route

--- src/app/budget/budget.routes.ts
+++ src/app/budget/budget.routes.ts
@@ -1,13 +1,20 @@
 import { Routes } from '@angular/router';
+import { rolesGuard } from '../auth/guards/roles.guard';
+import { Role } from '../auth/models/logged-in-user';
+import { ItemApprovalComponent } from './pages/item-approval/item-approval.component';
 import { ItemEntryComponent } from './pages/item-entry/item-entry.component';
 import { ItemFormComponent } from './pages/item-form/item-form.component';
-import { ItemApprovalComponent } from './pages/item-approval/item-approval.component';
 
 export const routes: Routes = [
   { path: 'item-entry', component: ItemEntryComponent, title: 'Entry' },
   { path: 'item-add', component: ItemFormComponent, title: 'Add' },
   { path: 'item-edit/:id', component: ItemFormComponent, title: 'Edit' },
-  { path: 'item-approval', component: ItemApprovalComponent, title: 'Approval' }
+  {
+    path: 'item-approval',
+    component: ItemApprovalComponent,
+    title: 'Approval',
+    canActivate: [rolesGuard([Role.ADMIN, Role.MANAGER])] // add
+  }
 ];
 
 export default routes;

da1fc1dd29eedcbb393630272d8267d1572f6221
Kiratipat sawangsisombat <capacity1412@gmail.com>
2024-10-22 12:26:11 +0700

    Refactor app.routes.ts to add auth component route

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,7 +1,6 @@
 import { Routes } from '@angular/router';
-import { DemoComponent } from './demo/demo.component';
 
 export const routes: Routes = [
-  { path: 'demo', component: DemoComponent },
-  { path: 'budget', loadChildren: () => import('./budget/budget.routes') }
+  { path: 'budget', loadChildren: () => import('./budget/budget.routes') },
+  { path: 'auth', loadChildren: () => import('./auth/auth.routes') }
 ];

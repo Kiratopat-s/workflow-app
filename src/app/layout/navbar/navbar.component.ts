7f736f05c1a4a7240e1ec330e1da7a8abbd35065
Kiratipat sawangsisombat <capacity1412@gmail.com>
2024-10-23 20:26:53 +0700

    Refactor navbar component to add lucide-angular package for SVG icons and update user profile display

--- src/app/layout/navbar/navbar.component.ts
+++ src/app/layout/navbar/navbar.component.ts
@@ -1,30 +1,41 @@
 import { Component, inject, OnInit } from '@angular/core';
 import { RouterLink } from '@angular/router';
 import { AuthService } from '../../auth/auth.service';
+import { LucideAngularModule, LogIn, KeyRound, LogOut, User, IdCard, Scroll, Stamp } from 'lucide-angular';
+
 
 @Component({
   selector: 'app-navbar',
   standalone: true,
-  imports: [RouterLink],
+  imports: [RouterLink, LucideAngularModule],
   templateUrl: './navbar.component.html',
   styleUrl: './navbar.component.scss'
 })
 export class NavbarComponent implements OnInit {
+  readonly icons = {
+    Login: LogIn,
+    Key: KeyRound,
+    Logout: LogOut,
+    User: User,
+    IdCard: IdCard,
+    Scroll: Scroll,
+    Stamp: Stamp
+  };
 
   authService = inject(AuthService);
-  menus: { path: string, title: string }[] = [];
+  menus: { path: string, title: string, icon: typeof LogIn }[] = [];
 
   ngOnInit() {
-    const userRole = this.authService.getCurrentProfile()?.role; // Assuming this method exists
-    if (userRole === 'USER') {
-      this.menus = [
-        { path: 'budget/item', title: 'List' }
-      ];
-    } else {
-      this.menus = [
-        { path: 'budget/item', title: 'List' },
-        { path: 'budget/item-approval', title: 'Approval' }
-      ];
+    this.menus = [
+      { path: 'budget/item', title: 'List', icon: this.icons.Scroll }
+    ];
+
+    const userRole = this.authService.getCurrentProfile()?.role;
+    console.log('User Role:', userRole);
+    if (userRole && userRole.toUpperCase() !== 'USER') {
+      this.menus.push(
+        { path: 'budget/item-approval', title: 'Approval', icon: this.icons.Stamp }
+      );
     }
   }
 

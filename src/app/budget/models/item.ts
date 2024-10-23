6d258f04cdf1e8664dec624c612567a1e68a3f38
Kiratipat sawangsisombat <capacity1412@gmail.com>
2024-10-23 09:57:01 +0700

    Refactor Item model to include owner and approver information

--- src/app/budget/models/item.ts
+++ src/app/budget/models/item.ts
@@ -2,23 +2,29 @@ export enum ItemStatus {
   PENDING = 'PENDING', APPROVED = 'APPROVED', REJECTED = 'REJECTED'
 }
 
-// export interface CreateItem {
-//   title: string;
-//   amount: number;
-//   price: number;
-//   contactMobileNo: string;
-//   status: ItemStatus;
-// }
 
 export type CreateItem = Omit<Item, "id">
 
 export type EditIem = CreateItem
 
+// "owner": {
+//             "id": 6,
+//             "username": "kirato"
+//         },
+export interface ItemUserRelation {
+  id: number;
+  username: string;
+}
 export interface Item {
   id: number;
   title: string;
   amount: number;
-  price: number;
-  contactMobileNo: string;
+  quantity: number;
   status: ItemStatus;
+  owner_id: number;
+  owner: ItemUserRelation;
+  approver_id: number | null;
+  approver: ItemUserRelation | null
+  created_at: Date;
+  updated_status_at: Date;
 }
